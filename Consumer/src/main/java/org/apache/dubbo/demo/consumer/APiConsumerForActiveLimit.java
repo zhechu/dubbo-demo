4b1cee97e701324765144549cc2aa16b607666bf
lingyuwang <ling-yu-wang@qq.com>
2020-12-12 13:49:07 +0800

    feature:消费端并发限制

--- Consumer/src/main/java/org/apache/dubbo/demo/consumer/APiConsumerForActiveLimit.java
+++ Consumer/src/main/java/org/apache/dubbo/demo/consumer/APiConsumerForActiveLimit.java
@@ -15,24 +15,23 @@ public class APiConsumerForActiveLimit {
 	public static void main(String[] args) {
 		ReferenceConfig<GreetingService> referenceConfig = new ReferenceConfig<GreetingService>();
 		referenceConfig.setApplication(new ApplicationConfig("first-dubbo-consumer"));
-		referenceConfig.setRegistry(new RegistryConfig("zookeeper://127.0.0.1:2181"));
+		referenceConfig.setRegistry(new RegistryConfig("zookeeper://192.168.1.103:2181"));
 		referenceConfig.setInterface(GreetingService.class);
 
 		referenceConfig.setVersion("1.0.0");
 		referenceConfig.setGroup("dubbo");
 		
-		//设置激活并发限制个数
-		referenceConfig.setActives(10);
-		
-//		final List<MethodConfig> methodList = new ArrayList<MethodConfig>();
-//
-//		MethodConfig methodConfig = new MethodConfig();
-//		methodConfig.setActives(10);
-//		methodConfig.setName("sayHello");
-//		methodList.add(methodConfig);
-//		referenceConfig.setMethods(methodList);
-		
-		
+		// 设置激活并发限制个数（每个方法最多同时并发请求5个）
+		referenceConfig.setActives(5);
+
+		// 设置具体方法激活并发限制个数（每个方法最多同时并发请求10个）
+		final List<MethodConfig> methodList = new ArrayList();
+		MethodConfig methodConfig = new MethodConfig();
+		methodConfig.setActives(10);
+		methodConfig.setName("sayHello");
+		methodList.add(methodConfig);
+		referenceConfig.setMethods(methodList);
+
 		GreetingService greetingService = referenceConfig.get();
 		
 		//设置隐式参数
