4672e6e114e8db35fb9f49084a65f7730954aa28
lingyuwang <ling-yu-wang@qq.com>
2020-12-12 21:21:57 +0800

    feature:消费端异步执行

--- Consumer/src/main/java/org/apache/dubbo/demo/consumer/APiAsyncConsumerForCompletableFuture2.java
+++ Consumer/src/main/java/org/apache/dubbo/demo/consumer/APiAsyncConsumerForCompletableFuture2.java
@@ -16,7 +16,7 @@ public class APiAsyncConsumerForCompletableFuture2 {
 		// 1.创建服务引用对象，并设置数据
 		ReferenceConfig<GreetingService> referenceConfig = new ReferenceConfig<GreetingService>();
 		referenceConfig.setApplication(new ApplicationConfig("first-dubbo-consumer"));
-		referenceConfig.setRegistry(new RegistryConfig("zookeeper://192.168.1.102:2181"));
+		referenceConfig.setRegistry(new RegistryConfig("zookeeper://192.168.1.103:2181"));
 		referenceConfig.setInterface(GreetingService.class);
 		referenceConfig.setTimeout(30000);
 		referenceConfig.setVersion("1.0.0");
@@ -27,17 +27,37 @@ public class APiAsyncConsumerForCompletableFuture2 {
 
 		// 3. 直接返回null
 		GreetingService greetingService = referenceConfig.get();
-		System.out.println(greetingService.sayHello("world"));
+		System.out.println(greetingService.sayHello("world1"));
 
 		// 4.异步执行回调
-		CompletableFuture<String> future = RpcContext.getContext().getCompletableFuture();
-		future.whenComplete((v, e) -> {
-			if (e != null) {
-				e.printStackTrace();
-			} else {
-				System.out.println(v);
-			}
-		});
+		CompletableFuture<String> future1 = RpcContext.getContext().getCompletableFuture();
+//		future1.whenComplete((v, e) -> {
+//			if (e != null) {
+//				e.printStackTrace();
+//			} else {
+//				System.out.println(v);
+//			}
+//		});
+
+		// 3. 直接返回null
+		greetingService = referenceConfig.get();
+		System.out.println(greetingService.sayHello("world2"));
+
+		// 4.异步执行回调
+		CompletableFuture<String> future2 = RpcContext.getContext().getCompletableFuture();
+//		future2.whenComplete((v, e) -> {
+//			if (e != null) {
+//				e.printStackTrace();
+//			} else {
+//				System.out.println(v);
+//			}
+//		});
+
+		String result1 = future1.get();
+		System.out.println(result1);
+
+		String result2 = future2.get();
+		System.out.println(result2);
 
 		System.out.println("over");
 		Thread.currentThread().join();
